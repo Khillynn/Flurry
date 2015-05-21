c4afb0c333f18b89a9bb7c6e863bd5ea882fd75d
Khillynn Thyme <khillynn.thyme@gmail.com>
2015-05-20 23:46:19 -0400

    The basics of everything is set up, just the core of the game is left.

--- src/main/java/com/khillynn/IcyFlurry.java
+++ src/main/java/com/khillynn/IcyFlurry.java
@@ -1,6 +1,7 @@
 package com.khillynn;
 
 import org.bukkit.*;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
@@ -9,10 +10,17 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.Vector;
 
 public class IcyFlurry extends JavaPlugin implements Listener {
     @Override
@@ -28,40 +36,60 @@ public class IcyFlurry extends JavaPlugin implements Listener {
                 second++;
 
                 if(second == 30)
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.YELLOW + "30 seconds until the round begins");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.YELLOW + "30 seconds until the round begins");
                 else if(second == 45)
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.YELLOW + "15 seconds");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.YELLOW + "15 seconds");
 
                 else if(second == 55){
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.RED + "5");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.RED + "5");
                     Bukkit.getWorld("Ice_Game_Map").playSound(loc, Sound.CLICK, 1, 0);
                 }
                 else if(second == 56){
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.RED + "4");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.RED + "4");
                     Bukkit.getWorld("Ice_Game_Map").playSound(loc, Sound.CLICK, 1, 0);
                 }
                 else if(second == 57){
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.RED + "3");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.RED + "3");
                     Bukkit.getWorld("Ice_Game_Map").playSound(loc, Sound.CLICK, 1, 0);
                 }
                 else if(second == 58){
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.RED + "2");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.RED + "2");
                     Bukkit.getWorld("Ice_Game_Map").playSound(loc, Sound.CLICK, 1, 0);
                 }
                 else if(second == 59){
-                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server:" + ChatColor.WHITE + "] " + ChatColor.RED + "Don't Die.");
+                    Bukkit.broadcastMessage("[" + ChatColor.GOLD + "Server" + ChatColor.WHITE + "]: " + ChatColor.RED + "Don't Die.");
                     Bukkit.getWorld("Ice_Game_Map").playSound(loc, Sound.CLICK, 1, 0);
                 }
 
                 else if(second == 60)
-                    Bukkit.getServer().getWorld("Ice_Game_Map").playEffect(Bukkit.getServer().getWorld("Ice_Game_Map").getSpawnLocation(), Effect.RECORD_PLAY, Material.getMaterial(2257));
+                    Bukkit.getServer().getWorld("Ice_Game_Map").playEffect(Bukkit.getServer().getWorld("Ice_Game_Map").getSpawnLocation(), Effect.RECORD_PLAY, Material.RECORD_4);
 
                 //stop music and drop death
-                else if(second == 80) {
+                else if(second == 70) {
                     Bukkit.getServer().getWorld("Ice_Game_Map").playEffect(Bukkit.getServer().getWorld("Ice_Game_Map").getSpawnLocation(), Effect.RECORD_PLAY, 0);
+                    Bukkit.getServer().getWorld("Ice_Game_Map").playSound(Bukkit.getServer().getWorld("Ice_Game_Map").getSpawnLocation(), Sound.AMBIENCE_RAIN, 1, 2);
+
+                    for(int x = 1; x <= 20; x++) {
+                        Snowball death = (Snowball) Bukkit.getWorld("Ice_Game_Map").spawnEntity(new Location(Bukkit.getWorld("Ice_Game_Map"), 0, 10, 0), EntityType.SNOWBALL);
+
+                        death.setVelocity(new Vector(0, -1, 0));
+                    }
                 }
             }
         }, 0L, 20L);
+    }
+
+    //turns the player towards the White Horseman, Death
+    @EventHandler
+    public void onJoin (PlayerJoinEvent e){
+        e.getPlayer().getLocation().setYaw(-89);
+        e.getPlayer().getLocation().setPitch(-50);
+    }
+
+    @EventHandler
+    public void onRespawn (PlayerRespawnEvent e){
+        e.getPlayer().getLocation().setYaw(-89);
+        e.getPlayer().getLocation().setPitch(-50);
     }
 
     //stops weather from changing from the default in the world
@@ -90,11 +118,26 @@ public class IcyFlurry extends JavaPlugin implements Listener {
 
     //players are hurt when they are in water
     @EventHandler
-    public void playerInWater (PlayerMoveEvent e){
+    public void playerInWater (PlayerMoveEvent e) {
         Material maybeWater = e.getTo().getBlock().getType();
+        Material maybeMoreWater = e.getFrom().getBlock().getType();
 
-        if(maybeWater.equals(Material.WATER) || maybeWater.equals(Material.STATIONARY_WATER))
-                e.getPlayer().damage(6.0);
+        if (maybeWater.equals(Material.WATER) || maybeWater.equals(Material.STATIONARY_WATER) || maybeMoreWater.equals(Material.WATER) || maybeMoreWater.equals(Material.STATIONARY_WATER)){
+            //e.getPlayer().damage(6.0);
+            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.POISON, 260, 3));
+        }
+    }
+
+    //prevents players from regaining health
+    @EventHandler
+    public void playerHPRegen (EntityRegainHealthEvent e){
+        e.setCancelled(true);
+    }
+
+    //prevents players from becoming hungry
+    @EventHandler
+    public void playerHungerStop (FoodLevelChangeEvent e){
+        e.setCancelled(true);
     }
 
     //when the player is hit by a snowball they'll die
